a18b67fbf32258ecd74cc9e8e7e9cc2a22abb9f8
arkeve <sketchthedev@gmail.com>
2021-03-18 22:10:59 -0400

    Update inventory visuals when it is open

--- PlayerInventory.gd
+++ PlayerInventory.gd
@@ -12,22 +12,34 @@ var inventory = {
 }
 
 func add_item(item_name, item_quantity):
-	for item in inventory:
+	var slot_indices: Array = inventory.keys()
+	slot_indices.sort()
+	for item in slot_indices:
 		if inventory[item][0] == item_name:
 			var stack_size = int(JsonData.item_data[item_name]["StackSize"])
 			var able_to_add = stack_size - inventory[item][1]
 			if able_to_add >= item_quantity:
 				inventory[item][1] += item_quantity
+				update_slot_visual(item, inventory[item][0], inventory[item][1])
 				return
 			else:
 				inventory[item][1] += able_to_add
+				update_slot_visual(item, inventory[item][0], inventory[item][1])
 				item_quantity = item_quantity - able_to_add
 	
 	# item doesn't exist in inventory yet, so add it to an empty slot
 	for i in range(NUM_INVENTORY_SLOTS):
 		if inventory.has(i) == false:
 			inventory[i] = [item_name, item_quantity]
+			update_slot_visual(i, inventory[i][0], inventory[i][1])
 			return
+
+func update_slot_visual(slot_index, item_name, new_quantity):
+	var slot = get_tree().root.get_node("/root/World/UserInterface/Inventory/GridContainer/Slot" + str(slot_index + 1))
+	if slot.item != null:
+		slot.item.set_item(item_name, new_quantity)
+	else:
+		slot.initialize_item(item_name, new_quantity)
 
 func remove_item(slot: SlotClass):
 	inventory.erase(slot.slot_index)
