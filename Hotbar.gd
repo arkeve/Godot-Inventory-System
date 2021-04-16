f454c72346f3bcdb8c1925bbbd86c9aea1e12b3b
arkeve <sketchthedev@gmail.com>
2021-04-15 20:05:39 -0400

    Add active item label

--- Hotbar.gd
+++ Hotbar.gd
@@ -2,18 +2,27 @@ extends Node2D
 
 const SlotClass = preload("res://Slot.gd")
 onready var hotbar_slots = $HotbarSlots
+onready var active_item_label = $ActiveItemLabel
 onready var slots = hotbar_slots.get_children()
 
 func _ready():
 #	for i in range(slots.size()):
 #		slots[i].connect("gui_input", self, "slot_gui_input", [slots[i]])
 #		slots[i].slot_index = i
+	PlayerInventory.connect("active_item_updated", self, "update_active_item_label")
 	for i in range(slots.size()):
 		PlayerInventory.connect("active_item_updated", slots[i], "refresh_style")
 		slots[i].connect("gui_input", self, "slot_gui_input", [slots[i]])
 		slots[i].slotType = SlotClass.SlotType.HOTBAR
 		slots[i].slot_index = i
 	initialize_hotbar()
+	update_active_item_label()
+
+func update_active_item_label():
+	if slots[PlayerInventory.active_item_slot].item != null:
+		active_item_label.text = slots[PlayerInventory.active_item_slot].item.item_name
+	else:
+		active_item_label.text = ""
 
 func initialize_hotbar():
 	for i in range(slots.size()):
@@ -37,6 +46,7 @@ func slot_gui_input(event: InputEvent, slot: SlotClass):
 						left_click_same_item(slot)
 			elif slot.item:
 				left_click_not_holding(slot)
+			update_active_item_label()
 
 func left_click_empty_slot(slot: SlotClass):
 	PlayerInventory.add_item_to_empty_slot(find_parent("UserInterface").holding_item, slot, true)
